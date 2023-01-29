e0011db1914ca9fd5f3641a55a8154af8a556426
kssrao13882 <kssrao13882@gmail.com>
2023-01-29 18:14:04 +0530

    RMX1971: OctaviOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1971.mk
+    $(LOCAL_DIR)/octavi_RMX1971.mk
+
+COMMON_LUNCH_CHOICES := \
+    octavi_RMX1971-user \
+    octavi_RMX1971-userdebug \
+    octavi_RMX1971-eng
